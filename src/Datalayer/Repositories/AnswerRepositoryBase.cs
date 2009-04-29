6a09569f2fb35f266d0fb0d613f575bd9a3ede42
Christof Sirk <sirchb01@gmail.com>
2009-04-29 18:35:01 +0200

    refactoring

---
+++ src/Datalayer/Repositories/AnswerRepositoryBase.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+using Domain.Repositories;
+using NHibernate;
+
+namespace TheNewEngine.Datalayer.Repositories
+{
+    public class AnswerRepositoryBase<T> : IRepository<T>
+    {
+        private readonly ISession mSession;
+
+        public AnswerRepositoryBase(ISession session)
+        {
+            mSession = session;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Enumerable.Cast<T>(mSession.CreateCriteria(typeof(T)).List());
+        }
+
+        public void Insert(T item)
+        {
+            mSession.Save(item);
+            mSession.Flush();
+        }
+
+        protected IEnumerable<T> GetAnswersFor(AnswerType answerType,
+            Func<Question, QuestionStage, T> create)
+        {
+            return mSession.CreateQuery(
+                "select q, s " +
+                "from QuestionStage as s " +
+                "inner join s.Question as q " +
+                "where q.AnswerType = :answer_type")
+                .SetParameter("answer_type", (int)AnswerType.Binary)
+                .Enumerable().Cast<object>().Select(
+                x =>
+                {
+                    var question = (Question)((object[])x)[0];
+                    var stage = (QuestionStage)((object[])x)[1];
+
+                    return create(question, stage);
+                }).ToArray();
+        }
+    }
+}
