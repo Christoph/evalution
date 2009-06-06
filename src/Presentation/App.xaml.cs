681942c14e490e4dba5ba9b508ef96547a9f0f05
Christof Sirk <sirchb01@gmail.com>
2009-06-06 23:13:03 +0200

    Moved setup code for DI to App.xaml.cs. Added the three main controls directly into the MainWindow.xaml which should make it easier to work with blend.

--- src/Presentation/App.xaml.cs
+++ src/Presentation/App.xaml.cs
@@ -4,6 +4,12 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using Domain;
+using Domain.Repositories;
+using NHibernate;
+using Ninject;
+using TheNewEngine.Datalayer;
+using TheNewEngine.Datalayer.Repositories;
 
 namespace Presentation
 {
@@ -12,5 +18,30 @@ namespace Presentation
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            InitializeDependencies();
+        }
+
+        private void InitializeDependencies()
+        {
+            IKernel kernel = new StandardKernel(new DatalayerModule(), new DomainModule());
+
+            DependencyResolver.InitializeWith(kernel);
+
+            var session = DependencyResolver.Resolve<ISession>();
+
+            var binaryAnswerRepository = new BinaryAnswerRepository(session);
+            var songAnswerRepository = new SongAnswerRepository(session);
+            var textAnswerRepository = new TextAnswerRepository(session);
+            var gradeAnswerRepository = new GradeAnswerRepository(session);
+            var formRepository = new FormRepository(session);
+            kernel.Bind<IQuestionFormRepository>().ToConstant(formRepository);
+
+            var formFactory = new FormFactory(formRepository, gradeAnswerRepository, textAnswerRepository, binaryAnswerRepository, songAnswerRepository);
+            var currentFormHolder = new CurrentFormHolder(formRepository.GetLast(), formFactory);
+
+            kernel.Bind<ICurrentFormHolder>().ToConstant(currentFormHolder);
+        }
     }
 }
--- src/Presentation/MainWindow.xaml.cs
+++ src/Presentation/MainWindow.xaml.cs
@@ -1,11 +1,5 @@
 using System.Windows;
 using Domain;
-using Domain.Repositories;
-using NHibernate;
-using Ninject;
-using Presentation.View;
-using TheNewEngine.Datalayer;
-using TheNewEngine.Datalayer.Repositories;
 
 namespace Presentation
 {
@@ -14,55 +8,15 @@ namespace Presentation
     /// </summary>
     public partial class MainWindow : Window
     {
-        private IQuestionFormRepository mFormRepository;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            InitializeDependencies();
+            var formViewModel = DependencyResolver.Resolve<QuestionFormViewModel>();
+            FormView.DataContext = formViewModel;
 
-            var answerControl = new AnswerControl();
-
-            //QuestionFormView
-            var formView = new QuestionFormView();
-
-            var currentFormHolder = DependencyResolver.Resolve<ICurrentFormHolder>();
-            var formViewModel = new QuestionFormViewModel(currentFormHolder, mFormRepository);
-
-            formView.DataContext = formViewModel;
-
-            var buttonsView = new ButtonsView();
-            var buttonsViewModel = new ButtonsViewModel(currentFormHolder, mFormRepository);
-            buttonsView.DataContext = buttonsViewModel;
-
-            TopContent.Children.Add(buttonsView);
-
-            CenterContent.Children.Add(formView);
-
-            BottomContent.Children.Add(answerControl);
-        }
-
-        private void InitializeDependencies()
-        {
-            IKernel kernel = new StandardKernel(new DatalayerModule(), new DomainModule());
-
-            DependencyResolver.InitializeWith(kernel);
-
-            var session = DependencyResolver.Resolve<ISession>();
-
-            var binaryAnswerRepository = new BinaryAnswerRepository(session);
-            var songAnswerRepository = new SongAnswerRepository(session);
-            var textAnswerRepository = new TextAnswerRepository(session);
-            var gradeAnswerRepository = new GradeAnswerRepository(session);
-            mFormRepository = new FormRepository(session);
-
-            var formFactory = new FormFactory(mFormRepository,
-                gradeAnswerRepository, textAnswerRepository, binaryAnswerRepository, songAnswerRepository);
-
-            var currentFormHolder = new CurrentFormHolder(mFormRepository.GetLast(), formFactory);
-
-            kernel.Bind<ICurrentFormHolder>().ToConstant(currentFormHolder);
+            var buttonsViewModel = DependencyResolver.Resolve <ButtonsViewModel>();
+            ButtonsView.DataContext = buttonsViewModel;
         }
     }
 }
